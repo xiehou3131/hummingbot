31e3fc52f366254d4616bc69f90dbd6342f0ea5a
posaggen <fan.wang@confluxnetwork.org>
2022-08-12 16:45:14 +0800

    feat: fix bug

--- hummingbot/connector/exchange/mexo/mexo_api_order_book_data_source.py
+++ hummingbot/connector/exchange/mexo/mexo_api_order_book_data_source.py
@@ -72,7 +72,7 @@ class MexoAPIOrderBookDataSource(OrderBookTrackerDataSource):
             symbols = []
             for trading_pair in self._trading_pairs:
                 symbol = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
-                symbols.append(f"{symbol.lower()}")
+                symbols.append(f"{symbol.upper()}")
             payload = {
                 "symbol": ','.join(symbols),
                 "topic": "trade",
